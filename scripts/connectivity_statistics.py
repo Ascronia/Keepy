36efd52bf116b0e595b22e4c8bd431c647d5cc44
Stephan Gerhard <git@unidesign.ch>
2011-02-09 21:12:25 +0100

    Added package function to script

--- scripts/connectivity_statistics.py
+++ scripts/connectivity_statistics.py
@@ -1,4 +1,3 @@
-import keepy.converter as kc
 import os
 import numpy as np
 from glob import glob
@@ -14,6 +13,71 @@ VP = ['%02i' % v for v in VP]
 
 bigarray = np.zeros( (len(Meas), len(Cond), len(VP), 19, 19, 8), dtype = np.float )
 
+
+def extract_metadata(filename):
+    """ Extract metadata, namely number of electrodes and nr of matrices
+    and band type """
+        
+    f = open(filename, 'r')
+    for line in f:
+        lsp = line.split(' = ')
+        if len(lsp) == 1:
+            continue
+        elif lsp == '':
+            break
+
+        if 'Number Electrodes' in lsp[1] or 'Number of regions of interest' in lsp[1]:
+            nr_elect = int(lsp[0])
+        elif 'Number of matrices' in lsp[1]:
+            nr_mat = int(lsp[0])
+        
+    f.close()
+    return nr_elect, nr_mat
+
+def extract_matrices(filename, enforce_nr_matrices = None):
+    """ Extracts a matrix (N,N,M) from a sLORETA connectivity file where
+    N is the number of ROIs or electrodes, and M is the number of frequency bands
+    """
+    
+    nr_elect, nr_mat = extract_metadata(filename)
+    
+    if not enforce_nr_matrices is None:
+        nr_mat = enforce_nr_matrices
+
+    result_matrix = np.zeros( (nr_elect,nr_elect, nr_mat), dtype = np.float )
+                
+    f = open(filename, 'r')
+    
+    # skip header
+    a = f.readline()
+    while ' = ' in a  or 'File name' in a or a == '':
+        a = f.readline()
+        
+    for mat_i in range(nr_mat):
+       # print 'Matrix: ', str(mat_i)
+        
+        for elect_i in range(nr_elect):
+           # print 'ROI:', str(elect_i)
+            line = f.readline()
+           # print "line", line
+
+            lsp = line.split('  ')
+            try:
+                lsp.remove('') # remove first empty character
+            except ValueError:
+                break
+
+            assert len(lsp) == nr_elect
+            
+            result_matrix[elect_i, :, mat_i] = np.array(lsp)
+            
+        line = f.readline()     
+                        
+    f.close()
+    
+    return result_matrix
+
+
 def retfn(ik, jc, stat):
     return '%s-%s-%s.csv' % (ik, jc, stat)
 
@@ -25,7 +89,7 @@ def generate_array():
                 fname = 'VP%s%s*-%s.txt' % (kv, jc, ik)
                 newpath = os.path.join(folder, ik, jc)
                 filename = glob(os.path.join(newpath, fname))[0]
-                bigarray[i, j, k, :, :, :] = kc.extract_matrices(filename, enforce_nr_matrices = 8)
+                bigarray[i, j, k, :, :, :] = extract_matrices(filename, enforce_nr_matrices = 8)
                 
     return bigarray
             
